1b52820e23707bf21cf5bf507ba2e3a55dea06d1
SuperMiaou <chante.matthieu@gmail.com>
2015-04-28 17:23:49 +0200

    First commit

---
+++ android/src/fr/CourBizu/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fr.CourBizu.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fr.CourBizu.game.MainActivity;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainActivity(), config);
+	}
+}
