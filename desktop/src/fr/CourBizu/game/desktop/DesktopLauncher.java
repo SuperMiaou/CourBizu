fea630e2d07a7e61568425ec857b3f5a25278c30
SuperMiaou <chante.matthieu@gmail.com>
2015-04-29 08:36:23 +0200

    Second commit

---
+++ desktop/src/fr/CourBizu/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.CourBizu.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.CourBizu.game.MainActivity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainActivity(), config);
+	}
+}
